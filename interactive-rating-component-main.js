04b2540fca1ed8dbea5d4647e742fb53b2dc6c3b
red0ahlam <109111970+red0ahlam@users.noreply.github.com>
2022-12-11 15:03:02 +0100

    Update interactive-rating-component-main.js

--- interactive-rating-component-main.js
+++ interactive-rating-component-main.js
@@ -8,32 +8,35 @@ const error = document.querySelector('.error');
 function pressed(e) {
     for (let i = 0; i < star_number.length; i++) {
         if (star_number[i] == e.target) {
-            star_number[i].setAttribute("aria-pressed", "true");
-            selected.innerHTML = i+1;
-        } else {
-            star_number[i].setAttribute("aria-pressed", "false");
+            selected.innerHTML = i + 1;
         }
     }
 }
 
 function checkPressed() {
     for (let i = 0; i < star_number.length; i++) {
-        if (star_number[i].getAttribute("aria-pressed") == 'true') {
+        if (star_number[i].checked) {
             return true;
         }
     }
     return false;
 }
 
+function hideError () {
+    error.classList.add("display");
+}
+
 for (let i = 0; i < star_number.length; i++) {
     star_number[i].addEventListener("click", pressed);
 }
 
-submit_btn.addEventListener("click", () => {
-    
-      if (checkPressed()) {
+submit_btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (checkPressed()) {
         thank_you_component.classList.remove("display");
     } else {
         error.classList.remove("display");
+        setTimeout(hideError, 1500);
     }
+
 })
